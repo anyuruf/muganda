ebcdd8f9c03947412b4b0d1fb3485ae93a7b3e20
anyuruf <anyuruf@gmail.com>
2021-12-03 20:41:37 +0300

    Feature - Installed and configured tailwind Css

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./app/**/*.html', './app/**/*.hbs'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      secondary: '#244c60',
+      charcoal: '#384640',
+      cream: '#eff4f0',
+      primary: '#970c10',
+    },
+  },
+  plugins: [],
+};
